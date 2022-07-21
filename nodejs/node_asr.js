ef537055693365f6aea214673a08e855c1eeca1d
ming@zmeet <jiaozhu@abcpen.com>
2022-07-21 16:29:17 +0800

    nodejs version

---
+++ nodejs/node_asr.js
@@ -0,0 +1,124 @@
+/**
+ *
+ * 运行前：请先填写Appid、APISecret
+ *
+ * 实时转写调用demo
+ * 此demo只是一个简单的调用示例，不适合用到实际生产环境中
+ * 
+ * @author zmeet
+ *
+ */
+const CryptoJS = require('crypto-js')
+const WebSocket = require('ws')
+var fs = require('fs')
+var log = require('log4node')
+var path = require("path")
+const { exit } = require('process')
+
+// 系统配置
+const config = {
+  // 请求地址
+  hostUrl: "wss://ai.abcpen.com/v1/ws",
+  //在控制台-我的应用-实时语音转写获取
+  appid: "",
+  //在控制台-我的应用-实时语音转写获取
+  apiKey: "",
+  file: path.dirname(__filename) + "/test_1.pcm",//请填写您的音频文件路径
+  highWaterMark: 6400
+}
+
+if (config.apiKey.length <= 0 || config.apiKey.length <= 0) {
+  log.error("api id and api secret empty")
+  exit()
+}
+
+
+// 获取当前时间戳
+let ts = parseInt(new Date().getTime() / 1000)
+
+let wssUrl = config.hostUrl + "?appid=" + config.appid + "&ts=" + ts + "&signa=" + getSigna(ts)
+let ws = new WebSocket(wssUrl)
+
+// 连接建立完毕，读取数据进行识别
+ws.on('open', (event) => {
+  log.info("websocket connect!")
+})
+
+// 得到识别结果后进行处理，仅供参考，具体业务具体对待
+let rtasrResult = []
+ws.on('message', (data, err) => {
+  if (err) {
+    log.info(`err:${err}`)
+    return
+  }
+  let res = JSON.parse(data)
+  switch (res.action) {
+    case 'error':
+      log.info(`error code:${res.code} desc:${res.desc}`)
+      break
+    // 连接建立
+    case 'started':
+      log.info('started!')
+      log.info('sid is:' + res.sid)
+      // 开始读取文件进行传输
+      var readerStream = fs.createReadStream(config.file, {
+        highWaterMark: config.highWaterMark
+      });
+      readerStream.on('data', function (chunk) {
+        ws.send(chunk)
+      });
+      readerStream.on('end', function () {
+        // 最终帧发送结束
+        ws.send("{\"end\" : true}")
+      });
+      break
+    case 'result':
+      // ... do something
+      //if (res.data)
+      try {
+        //let data = JSON.parse(res.data)
+        let data = res.data
+        if (!data.cn)
+          break;
+        rtasrResult[data.seg_id] = data
+        // 把转写结果解析为句子
+        if (data.cn.st.type == 0) {
+          rtasrResult.forEach(i => {
+            let str = "实时转写"
+            str += (i.cn.st.type == 0) ? "【最终】识别结果：" : "【中间】识别结果："
+            i.cn.st.rt.forEach(j => {
+              j.ws.forEach(k => {
+                k.cw.forEach(l => {
+                  str += l.w
+                })
+              })
+            })
+            log.info(str)
+          })
+
+        }
+      } catch (e) {
+        console.log("meet exception: ", e)
+      }
+
+      break
+  }
+})
+
+// 资源释放
+ws.on('close', () => {
+  log.info('connect close!')
+})
+
+// 建连错误
+ws.on('error', (err) => {
+  log.error("websocket connect err: " + err)
+})
+
+// 鉴权签名
+function getSigna(ts) {
+  let md5 = CryptoJS.MD5(config.appid + ts).toString()
+  let sha1 = CryptoJS.HmacSHA1(md5, config.apiKey)
+  let base64 = CryptoJS.enc.Base64.stringify(sha1)
+  return encodeURIComponent(base64)
+}
---
+++ nodejs/readme.txt
@@ -0,0 +1,6 @@
+NODEJS 集成步骤
+1：打开NodeJS官网https://nodejs.org/en/下载windows系统对应的LTS版本；
+2：下载可执行文件之后，选择路径安装，安装完毕后，打开cmd输入node -v和npm -v如果正确输出版本号说明已安装完成。
+3：下载官网node js demo解压至相应目录，打开js文件添加自己的appid及相应的信息，打开cmd切换至所下载nodejs_demo的根目录：
+   运行命令：①  "npm install"		
+	   ②  "node XXXX.js"
